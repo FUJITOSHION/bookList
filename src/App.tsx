023332dcb980938939b2177c73779d156d160229
fujitoshion <shion@shion.local>
2021-03-03 01:16:10 +0900

    refactor

--- src/App.tsx
+++ src/App.tsx
@@ -23,27 +23,6 @@ const customStyles = {
   },
 }
 
-const dummyBooks: BookToRead[] = [
-  {
-    id: 1,
-    title: 'はじめてのReact',
-    authors: 'ダミー',
-    memo: '',
-  },
-  {
-    id: 2,
-    title: 'React Hooks入門',
-    authors: 'ダミー',
-    memo: '',
-  },
-  {
-    id: 3,
-    title: '実践Reactアプリケーション開発',
-    authors: 'ダミー',
-    memo: '',
-  },
-]
-
 const APP_KEY = 'react-book'
 
 export const App: React.FC = () => {
